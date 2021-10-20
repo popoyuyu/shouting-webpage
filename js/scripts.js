7358a93b2359a8e3db91efcce121dc9feb2731c4
mellowmorgan <morgan.waites@gmail.com>
2021-10-20 10:22:21 -0700

    DEBUG: Fix jqeury
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $(#formOne).submit(function(event){
+  $("#formOne").submit(function(event){
     event.preventDefault();
-    let input = $(#input1).val();
+    let input = $("#input1").val();
     input = input.toUpperCase();
-    alert("hi");
+    alert(input);
   
   });
 });
